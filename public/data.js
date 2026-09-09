a53eeda7b79a8adf9f53094bedf7aa5126526455
Fonsonis <daniel290188@gmail.com>
2026-09-09 13:19:31 +0200

    Lower Luffy Gear 3 evolution level to 25

--- public/data.js
+++ public/data.js
@@ -197,8 +197,8 @@ const CHARS = {
     name: 'Luffy Gear 2', emoji: '👒', types: ['Golpe', 'Fruta'],
     base: [30, 16, 10, 15], rareza: 2,
     learnset: [[13, 'gatlinggoma'], [20, 'jetpistol'], [30, 'hakiarm']],
-    evo: { lvl: 35, to: 'luffy3' }, ultimate: 'jetgatling',
-    desc: 'Gear Second: velocidad y vapor. Gear Third al nivel 35.',
+    evo: { lvl: 25, to: 'luffy3' }, ultimate: 'jetgatling',
+    desc: 'Gear Second: velocidad y vapor. Gear Third al nivel 25.',
   },
   luffy3: {
     name: 'Luffy Gear 3', emoji: '👒', types: ['Golpe', 'Fruta'],
